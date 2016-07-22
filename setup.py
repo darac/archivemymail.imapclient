8389fb3143ca080778f4629eb02a7213bef203ad
Paul Saunders <darac@darac.org.uk>
2016-07-22 14:56:21 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -25,17 +25,17 @@ long_description = read('README.md', 'CHANGES.txt')
 
 
 setup(
-        name="archivemymail",
+    name="archivemymail",
     version='1.0',
-        author="Paul Saunders",
-        install_requires=['imapclient>=1.0',
-                          'pyyaml>=3.11',
-                          'appdirs>=1.4.0'],
-        author_email='darac@darac.org.uk',
-        description='Archive Mail to MBoxes, with filtering through Spam Learning',
-        long_description=long_description,
+    author="Paul Saunders",
+    install_requires=['imapclient>=1.0',
+                      'pyyaml>=3.11',
+                      'appdirs>=1.4.0'],
+    author_email='darac@darac.org.uk',
+    description='Archive Mail to MBoxes, with filtering through Spam Learning',
+    long_description=long_description,
     packages=['archivemymail', 'archivemymail.tests'],
-        platforms='any',
+    platforms='any',
     test_suite='archivemymail.tests.get_suite',
     scripts=['archivemymail.py'],
 )
