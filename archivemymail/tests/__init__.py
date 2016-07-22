7207317047ffc2b155fab5678b1bfa4f36c86329
Paul Saunders <darac@darac.org.uk>
2016-07-22 14:37:21 +0100

    Futurize!

--- archivemymail/tests/__init__.py
+++ archivemymail/tests/__init__.py
@@ -1,10 +1,11 @@
+from __future__ import absolute_import
 # implement basic tests
 import unittest
 
 def get_suite():
     "Return a unittest.TestSuite."
 
-    import test_StatsMan
+    from . import test_StatsMan
 
     loader = unittest.TestLoader()
     suite = loader.loadTestsFromModule(test_StatsMan)
